c818d63350a5db84638205b8dbb28062337f57d5
Badkarma11 <pranesh@example.com>
2026-03-10 18:42:04 +0530

    Optimize resume parsing using PyMuPDF

--- backend/resume_parser.py
+++ backend/resume_parser.py
@@ -1,41 +1,18 @@
-import pdfplumber
-import docx
-import re
-import warnings
-import logging
-logging.getLogger("pdfminer").setLevel(logging.ERROR)
-warnings.filterwarnings("ignore")
+import fitz  # PyMuPDF
 
 
 def extract_text(file_path):
-
     text = ""
 
     try:
-        if file_path.endswith(".pdf"):
-            with pdfplumber.open(file_path) as pdf:
-                for page in pdf.pages:
-                    extracted = page.extract_text()
-                    if extracted:
-                        text += extracted + "\n"
+        doc = fitz.open(file_path)
 
-        elif file_path.endswith(".docx"):
-            doc = docx.Document(file_path)
-            for para in doc.paragraphs:
-                text += para.text + "\n"
+        for page in doc:
+            text += page.get_text()
 
-        else:
-            raise ValueError("Unsupported file format")
+        doc.close()
 
     except Exception as e:
-        print("⚠ PDF extraction warning:", e)
-        return ""
+        print("Error reading PDF:", e)
 
-    return clean_text(text)
-
-
-def clean_text(text):
-    text = re.sub(r'[ \t]+', ' ', text)
-    text = re.sub(r'\n+', '\n', text)
-    text = text.lower()
-    return text.strip()
+    return text
